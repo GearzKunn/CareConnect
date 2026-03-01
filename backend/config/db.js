ad889091bfffcb535e99230cba697cf6b0b67c9b
KIRA Gearz <maharrshidas06@gmail.com>
2026-03-02 01:32:00 +0530

    added database

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "Gearz@97531",
-  database: "CareConnect"
+  database: "careconnect"
 });
 
 db.connect((err) => {
